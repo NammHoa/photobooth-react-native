22c235d8b29a484dc3c0bb9ec7c7d46d3a63afbf
Hoa Nam <namdeptrai270304@gmail.com>
2026-05-19 21:20:11 +0700

    Fix: completely remove mobile submodule and add source code

--- backend/server.js
+++ backend/server.js
@@ -8,22 +8,17 @@ const photoRoutes = require('./routes/photoRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Biến thư mục 'uploads' thành public để có thể truy cập bằng trình duyệt/app
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Routes
 app.use('/api/photos', photoRoutes);
 
-// Test Route
 app.get('/', (req, res) => {
   res.send('Photobooth API is running!');
 });
 
-// Kết nối MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
